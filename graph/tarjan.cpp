a6f3c45e119778c4770008238996a94d8602c8cd
Priyanshu Pradhan <118527847+Bully26@users.noreply.github.com>
2025-08-26 23:19:53 +0000

    tarjan

---
+++ graph/tarjan.cpp
@@ -0,0 +1,80 @@
+//Position this line where user code will be pasted.
+class Solution {
+  public:
+    int tarjan(vector<vector<int>> &adj) {
+        // code here
+        
+        // so this is going to be tarjans algorithm         // lets see what are thing we need to code  this
+        // low link / in stack / stack / timer 
+        
+        
+        // the basic logic is visit node push stack return the min time from child
+        // if min time from children which is commming back is min time of current node 
+        // cur node is cnenter of scc
+        
+        // stack help us to sepearte the current scc from going into other scc
+        
+        int inf = 1e8;
+        int n = adj.size();
+        vector<int> low,instack,entry;
+        deque<int> stack;
+        low.assign(n+1,inf);
+        entry.assign(n+1,-1);
+        instack.assign(n+1,0);
+        int tmr =0;
+        vector<vector<int>>scc;
+        
+        function <void(int)> dfs = [&](int cur)
+        {
+            if(low[cur]!=inf)
+            {
+                 return;
+            }
+            entry[cur]=low[cur]=tmr++;
+            instack[cur]=1;
+            stack.push_back(cur);
+            
+            for(int child:adj[cur])
+            {
+                if(low[child]==inf)
+                {
+                    dfs(child);
+                    low[cur]=min(low[cur],low[child]);
+                }else if (instack[child]==1)
+                {
+                    low[cur]=min(low[cur],low[child]);
+                }
+               
+            }
+            
+            if(low[cur]==entry[cur])
+            {
+               
+                vector<int> comp;
+                {
+                    int node = stack.back();
+                    comp.push_back(node);
+                    stack.pop_back();
+                    instack[node]=0;
+                    
+                    if(node == cur)break;
+                }
+                scc.push_back(comp);
+            }
+            
+            return ;
+        };
+        
+        for(int i=0;i<n;i++)
+        {
+            dfs(i);
+        }
+        
+        return scc.size();
+            
+        }
+        
+        
+        
+    
+};
