d5db35f8ab472bed2e4924126b0e4740a5935471
Priyanshu Pradhan <118527847+Bully26@users.noreply.github.com>
2025-09-06 11:57:35 +0530

    Update sparsetable.cpp

--- range_query/sparsetable.cpp
+++ range_query/sparsetable.cpp
@@ -1,38 +1,64 @@
-class sptable{
-   public:
-  vector<vector<int>> sp;
+#include <bits/stdc++.h>
+using namespace std;
+
+class spr{
+  public:
+  vector<vector<int>> tbl;
   
-  
-  template<typename F>
-  sptable(vector<int> &vec , F fun)
+  spr(vector<int> vec)
   {
+  	int n = vec.size();
+    int p2 = log2(n)+1;
     
-     int n = vec.size();
-     int p = log2(n)+1;
-     
-     sp.assign(p,vector<int> (n,-1));
-     
-     for(int i=0;i<n;i++)
-     {
-       sp[0][i]=vec[i];
-     }
-     
-     for(int k = 1; k <= p ;k++)
-     {
-        for(int i =0 ; i+ pow(2,k) -1  < n ; i++)
-        {
-          sp[k][i] = fun(sp[k-1][i] , sp[k-1][i + pow(2,k-1)]);
-        }
-     }
+    
+    tbl.assign(p2 ,vector<int> (n , 1e7));
+    
+    for(int i=0;i<n;i++)
+    {
+    	tbl[0][i] = vec[i]; 
+    }
+    
+    for(int p = 1; p < p2 ;p++)
+    {
+    	for(int i=0;i + (1<<p) <n; i++ )
+    	{
+    		tbl[p][i] = min(tbl[p-1][i] , tbl[p-1][i+ (1<<(p-1))]);
+    	}
+    }
+    
   }
-  template<typename F>
-  int query(int l, int r,F fun)
+  
+  int query(int l , int r)
   {
-     int len = r - l + 1;
-     int p = log2(len);
-     
-     int res = fun(sp[p][l], sp[p][l + (len - pow(2,p))]);
-     
-     return res;
+  	
+  	int len  = r - l +1 ;
+  	int p = log2(len);
+  	
+  	return min(tbl[p][l] , tbl[p][r - (1<<p) +1]);
+  
   }
+	
+	
 };
+
+#define shw(x)   cout<<x<<endl;
+
+
+int main()
+{
+   vector<int> vec{12,3245,457,234,12,345,4567,112,12,42346,4231,4,5,2,1};
+   
+   spr m = spr(vec);
+   
+   
+   shw(m.query(0,4));
+   shw(m.query(2,4));
+   shw(m.query(5,6));
+   shw(m.query(6,7));
+   shw(m.query(6,6));
+   shw(m.query(6,7));
+   shw(m.query(8,8));
+   
+   
+   
+}
